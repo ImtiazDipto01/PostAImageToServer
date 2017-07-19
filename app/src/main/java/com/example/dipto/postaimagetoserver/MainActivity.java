70a1e3db4c44966d05f587eda2ecd7377dfe88ee
imtiaz <programmer.imtiaz@gmail.com>
2017-07-18 19:29:17 -0700

    UPDATE - DoInBackground, OnPostExecute, OnPreExecute

--- app/src/main/java/com/example/dipto/postaimagetoserver/MainActivity.java
+++ app/src/main/java/com/example/dipto/postaimagetoserver/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.dipto.postaimagetoserver;
 
+import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
@@ -8,13 +10,28 @@ import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Base64;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,6 +40,7 @@ public class MainActivity extends AppCompatActivity {
     ImageView UploadImageView ;
     private int PICK_IMAGE_REQUEST = 1;
     private Bitmap bitmap ;
+    String image_name, uploaded_image ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +52,18 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 chooseImageFile();
+            }
+        });
+
+        UploadImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                image_name = ImageName.getText().toString() ;
+                uploaded_image = getStringImage(bitmap) ;
+
+                Log.d("UPLOAD IMAGE ::::::::", String.valueOf(uploaded_image.length())) ;
+                ApiTaskUploadImage ApiCall = new ApiTaskUploadImage(MainActivity.this) ;
+                ApiCall.execute(uploaded_image, image_name) ;
             }
         });
     }
@@ -52,14 +82,6 @@ public class MainActivity extends AppCompatActivity {
         startActivityForResult(Intent.createChooser(intent, "Select Image"), PICK_IMAGE_REQUEST);
     }
 
-    private String getStringImage(Bitmap bitmap){
-        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream() ;
-        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, byteArrayOutputStream) ;
-        byte[] imagebyte = byteArrayOutputStream.toByteArray() ;
-        String encodeString = Base64.encodeToString(imagebyte, Base64.DEFAULT) ;
-        return encodeString ;
-    }
-
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
@@ -76,16 +98,80 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    class ApiTaskUploadImage extends AsyncTask<Void, Void, Void>{
+    private String getStringImage(Bitmap bitmap){
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream() ;
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, byteArrayOutputStream) ;
+        byte[] imagebyte = byteArrayOutputStream.toByteArray() ;
+        String encodeString = Base64.encodeToString(imagebyte, Base64.DEFAULT) ;
+        return encodeString ;
+    }
 
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
+    class ApiTaskUploadImage extends AsyncTask<String, Void, String>{
+
+        Context context ;
+        ProgressDialog progressDialog ;
+        String upload_image_url = "http://programmerimtiaz.000webhostapp.com/PhotoUploadWithText/upload.php" ;
+
+        ApiTaskUploadImage(Context context){
+            this.context = context ;
         }
 
         @Override
-        protected Void doInBackground(Void... params) {
-            return null;
+        protected void onPreExecute() {
+            progressDialog = new ProgressDialog(context) ;
+            progressDialog.setTitle("Please Wait");
+            progressDialog.setMessage("Image Uploading...");
+            progressDialog.setIndeterminate(true);
+            progressDialog.setCancelable(false);
+            progressDialog.show();
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+            StringBuilder stringBuilder = null;
+
+            try {
+                URL url = new URL(upload_image_url) ;
+                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+                httpURLConnection.setRequestMethod("POST");
+                httpURLConnection.setDoInput(true);
+                httpURLConnection.setDoOutput(true);
+
+                OutputStream outputStream = httpURLConnection.getOutputStream() ;
+                BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(outputStream, "UTF-8")) ;
+
+                String image = params[0];
+                String name = params[1];
+                String data = URLEncoder.encode("image", "UTF-8")+"="+URLEncoder.encode(image, "UTF-8")+"&"+
+                        URLEncoder.encode("name", "UTF-8")+"="+URLEncoder.encode(name, "UTF-8") ;
+
+                bufferedWriter.write(data);
+                bufferedWriter.flush();
+                bufferedWriter.close();
+                outputStream.close();
+
+
+                int responsecode = httpURLConnection.getResponseCode() ;
+                if(responsecode == HttpURLConnection.HTTP_OK){
+                    InputStream inputStream = httpURLConnection.getInputStream() ;
+                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream, "UTF-8")) ;
+                    stringBuilder = new StringBuilder() ;
+                    String line = "" ;
+                    while((line = bufferedReader.readLine()) != null){
+                        stringBuilder.append(line + "\n") ;
+                    }
+                }
+                httpURLConnection.disconnect();
+
+            }
+            catch (MalformedURLException e) {
+                Log.d("++++Malformed+++++", String.valueOf(e)) ;
+            }
+            catch (IOException e) {
+                Log.d("++++IOException+++++", String.valueOf(e)) ;
+            }
+
+            return stringBuilder.toString().trim();
         }
 
         @Override
@@ -94,8 +180,22 @@ public class MainActivity extends AppCompatActivity {
         }
 
         @Override
-        protected void onPostExecute(Void aVoid) {
-            super.onPostExecute(aVoid);
+        protected void onPostExecute(String json) {
+
+            try {
+                progressDialog.dismiss();
+                JSONObject mainJSONobj = new JSONObject(json.substring(json.indexOf("{"), json.lastIndexOf("}") + 1)) ;
+                Log.d("mainJSONobj=====", String.valueOf(mainJSONobj)) ;
+
+                String response = mainJSONobj.getString("response") ;
+                Log.d("RESPONSE=====", response) ;
+
+                Toast.makeText(MainActivity.this, response, Toast.LENGTH_SHORT) ;
+            }
+            catch (JSONException e) {
+                Log.d("++++JSONException+++++", String.valueOf(e)) ;
+            }
+
         }
     }
 }
